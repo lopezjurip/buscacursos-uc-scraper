eb9e00ef74a5a8650d9d52beefbea5907073bcec
Patricio López <lopezjuripatricio@gmail.com>
2015-12-23 10:38:53 -0300

    step 3 fix

--- third-step.js
+++ third-step.js
@@ -1,8 +1,25 @@
-const initials = require('./second-step');
+'use strict';
+
+let initials = require('./second-step');
+const buscacursos = require('buscacursos-uc');
+const MySet = require("collections/set");
+const async = require('async');
+
+function split(a, n) {
+  var len = a.length,
+    out = [],
+    i = 0;
+  while (i < len) {
+    var size = Math.ceil((len - i) / n--);
+    out.push(a.slice(i, i += size));
+  }
+  return out;
+}
 
 const page = process.argv[2] || 0;
 initials = split(initials, 10);
 initials = initials[page];
+console.log(initials.length);
 
 function ewe() {
   const queries = initials.map(i => {
