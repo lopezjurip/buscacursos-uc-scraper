01b999ba91a71c6c7cb0647b61b3c62a6a6e875f
Patricio López <lopezjuripatricio@gmail.com>
2015-12-25 11:13:57 -0300

    upgrade examples

--- example/all.js
+++ example/all.js
@@ -3,7 +3,26 @@
 const fs = require('fs');
 const scraper = require('../index');
 
+// This takes a lot of time to complete.
+// Also, creates heavy load on the server, use with caution.
+
+/*
 scraper.all(function(err, courses) {
+  if (err) return console.error(err);
+
+  console.log('Count:', courses.length);
+  fs.writeFile('./all.json', JSON.stringify(courses, null, 4), function(err) {
+    if (err) return console.error(err);
+
+    console.log('success!');
+  });
+});
+*/
+
+// Recomended:
+const initials = scraper.initials;
+
+scraper.deepSearch(initials, function(err, courses) {
   if (err) return console.error(err);
 
   console.log('Count:', courses.length);
