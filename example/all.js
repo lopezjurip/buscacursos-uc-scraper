85718d0538d20905762b31684fea34e8b33ebf1b
Patricio López <lopezjuripatricio@gmail.com>
2016-01-04 17:12:12 -0300

    promises instead of callbacks

--- example/all.js
+++ example/all.js
@@ -5,30 +5,27 @@ const scraper = require('../index');
 
 // This takes a lot of time to complete.
 // Also, creates heavy load on the server, use with caution.
+// Big chances of failing
 
-/*
-scraper.all({ year: 2016, period: 1}, function(err, courses) {
-  if (err) return console.error(err);
-
+scraper.all({ year: 2016, period: 1 }).then(courses => {
   console.log('Count:', courses.length);
   fs.writeFile('./all.json', JSON.stringify(courses, null, 4), function(err) {
     if (err) return console.error(err);
 
     console.log('success!');
   });
-});
-*/
+}).catch(console.error);
 
 // Recomended:
-const initials = scraper.initials;
-
-scraper.deepSearch(initials, { year: 2016, period: 1}, function(err, courses) {
-  if (err) return console.error(err);
-
-  console.log('Count:', courses.length);
-  fs.writeFile('./all.json', JSON.stringify(courses, null, 4), function(err) {
-    if (err) return console.error(err);
-
-    console.log('success!');
-  });
-});
+// const initials = scraper.initials;
+//
+// scraper.deepSearch(initials, { year: 2016, period: 1}, function(err, courses) {
+//   if (err) return console.error(err);
+//
+//   console.log('Count:', courses.length);
+//   fs.writeFile('./all.json', JSON.stringify(courses, null, 4), function(err) {
+//     if (err) return console.error(err);
+//
+//     console.log('success!');
+//   });
+// });
--- example/some.js
+++ example/some.js
@@ -3,13 +3,11 @@
 const fs = require('fs');
 const scraper = require('../index');
 
-scraper.deepSearch(['IIC'], { year: 2015, period: 2 }, function(err, courses) {
-  if (err) return console.error(err);
-
+scraper.deepSearch(['IIC', 'MAT'], { year: 2016, period: 1 }).then(courses => {
   console.log('Count:', courses.length);
   fs.writeFile('./some.json', JSON.stringify(courses, null, 4), function(err) {
     if (err) return console.error(err);
 
     console.log('success!');
   });
-});
+}).catch(console.error);
